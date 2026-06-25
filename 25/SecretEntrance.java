6f6b8a0de443c85c256098ba60c00d12af3e2ae3
yihune21 <zewdieyihune@gmail.com>
2026-06-25 15:24:49 +0300

    initial commit.

---
+++ 25/SecretEntrance.java
@@ -0,0 +1,18 @@
+class SecretEntrance {
+        int intialDial = 50;
+        int password = 0;
+	int getPassword(String[] instructions){
+
+	    }    
+
+	    return password;
+	}
+        
+ 	
+       
+	void main(){
+	   SecretEntrance se = new SecretEntrance();
+	   int password = se.getPassword(instructions);
+	   IO.println(password);
+	}
+}
