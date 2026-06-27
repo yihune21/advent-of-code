b560857f7d62d3d92af7191d7c43a854394ab6fc
yihune21 <zewdieyihune@gmail.com>
2026-06-27 19:30:25 +0300

    first aoc puzzle ever.

--- 25/SecretEntrance.java
+++ 25/SecretEntrance.java
@@ -1,18 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Formatter;
+
 class SecretEntrance {
-        int intialDial = 50;
+	int currentDial = 50;
         int password = 0;
-	int getPassword(String[] instructions){
-
-	    }    
-
+	int getPassword(String path){
+	    readFile(path);
 	    return password;
 	}
+
+	void leftParser(String instruction){
+	        String rotation = instruction.substring(1);
+	        int rot = Integer.parseInt(rotation);
+		currentDial = (currentDial - rot) % 100;
+                
+		if(currentDial < 0){
+		   currentDial += 100;
+		}
+
+		if (currentDial == 0){
+		   password += 1;
+		}	
+	}
+	void rightParser(String instruction){
+	        String rotation = instruction.substring(1);
+	        int rot = Integer.parseInt(rotation);
+                currentDial = (currentDial + rot) % 100;
+
+		if (currentDial == 0){
+		   password += 1;
+		}	
+	}
         
- 	
+	void readFile(String path){
+	    try(BufferedReader in = new BufferedReader(new FileReader(path))){
+		  String instruction = in.readLine();
+		  IO.println("File Reader Started");
+		  
+		  while(instruction != null ){
+		    if(instruction.startsWith("L")){
+			    leftParser(instruction);
+		    }else{
+		            rightParser(instruction);
+		    }
+		    instruction = in.readLine();
+		  }
+	    }catch(IOException e){
+	          e.printStackTrace();
+	    }
+	}
        
 	void main(){
 	   SecretEntrance se = new SecretEntrance();
-	   int password = se.getPassword(instructions);
-	   IO.println(password);
+	   int password = se.getPassword("inputs/day-01.txt");
+	   String strPassword =  String.format("The Password is: %d",password);
+	   IO.println(strPassword);
 	}
 }
